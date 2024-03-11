3af4be5d1ea63b3125e61b2441623ca972cb592c
Ulises Barrionuevo <ulitito54@hotmail.es>
2024-03-11 16:06:37 -0300

    a

---
+++ WebadmiClient/Migrations/202403111858107_CRMUpdate.cs
@@ -0,0 +1,32 @@
+﻿namespace WebadmiClient.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CRMUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Registers");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Registers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 60),
+                        Surname = c.String(nullable: false, maxLength: 60),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        Login = c.String(nullable: false, maxLength: 60),
+                        Password = c.String(nullable: false),
+                        RoleId = c.Int(nullable: false),
+                        IsDeleted = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
