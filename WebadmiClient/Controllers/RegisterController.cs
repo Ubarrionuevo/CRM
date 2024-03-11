4eea5c0f6bec198506124dd4849367b7dca7cd8c
Ulises Barrionuevo <ulitito54@hotmail.es>
2024-03-11 15:48:36 -0300

    h

---
+++ WebadmiClient/Controllers/RegisterController.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.Entity;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebadmiClient.Models;
+
+
+namespace WebadmiClient.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class RegisterController : Controller
+    {
+        private readonly ApDbContext _context;
+
+        public RegisterController()
+        {
+            _context = new ApDbContext();
+        }
+
+        // GET: Registro
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Register usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Register.Add(usuario);
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Index", "Login"); // Redirigir al usuario a la página de inicio de sesión
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error al registrar usuario: " + ex.Message);
+                }
+            }
+
+            return View(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+
+
+}
+
---
+++ WebadmiClient/Models/Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebadmiClient.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El campo Email no es una dirección de correo electrónico válida.")]
+        public string Email { get; set; }
+
+        // Otros campos del usuario, si los hay
+    }
+}
